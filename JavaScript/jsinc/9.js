ddfff62f5e686091ade7f35a3b2f3f0a13c64622
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2023-05-04 21:39:44 +0400

    js9

--- JavaScript/jsinc/9.js
+++ JavaScript/jsinc/9.js
@@ -5,3 +5,33 @@ const foo = () => {
 }
 foo()()
 
+function counter() {
+    let count = 0
+    const a = () => {
+        console.log(count++)
+    }
+    console.log(count)
+    return a
+}
+
+const count = counter()
+count()
+count()
+count()
+
+
+// function counter1() {
+//
+//     function a() {
+//         let count = 0
+//         console.log(count++)
+//     }
+//
+//     // console.log(count)
+//     return a
+// }
+//
+// const count1 = counter1()
+// count1()
+// count1()
+// count1()
