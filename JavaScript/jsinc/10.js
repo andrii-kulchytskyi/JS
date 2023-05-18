8a2c62944c2ea9a4ab5a0295b63585cfe48fb24a
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2023-05-18 23:31:28 +0400

    css

--- JavaScript/jsinc/10.js
+++ JavaScript/jsinc/10.js
@@ -15,3 +15,18 @@ const pr = server.getData().then((result) => {
 }).finally((e) => {
 
 })
+
+let obj = {x: 1, y: 2, z: 3};
+let squeredVal = Object.values(obj).map(i => i ** 2)
+console.log(squeredVal)
+console.log(Object.keys(obj).length)
+
+// delay(ms)
+function delay(ms) {
+    const prom = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(resolve())
+        }, ms)
+    })
+    return prom
+}
