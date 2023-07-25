2d929f31a06f51538875ee81cfdf020369f6b866
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2023-07-25 21:37:34 +0400

    css

--- JavaScript/pra/pra2/2.1.js
+++ JavaScript/pra/pra2/2.1.js
@@ -1486,3 +1486,21 @@
 //
 // console.log( func(10) );
 // console.log( func(-5) );
+
+// Wilson primes satisfy the following condition. Let P represent a prime number.
+//
+//     Then,
+//
+//     ((P-1)! + 1) / (P * P)
+// should give a whole number.
+//
+//     Your task is to create a function that returns true if the given number is a Wilson prime.
+
+// function amIWilson(p) {
+//     let a = 1
+//     for (let i = 1; i < p; i++) {
+//         a *= i
+//     }
+//     return (a+1)/(p*p)%1===0
+// }
+
