bb0d11ce713ad8084e85f196c1052e05ff24318b
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2023-09-01 23:18:46 +0400

    js

--- JavaScript/pra/pra2/2.1.js
+++ JavaScript/pra/pra2/2.1.js
@@ -2453,3 +2453,17 @@
 //
 // Сделайте функцию func1, которая будучи вызвана вот так: func1()(), вернет число 1.
 // Сделайте аналогичную функцию func2, возвращающую число 2. Найдите сумму результатов этих функций.
+
+function func1() {
+    return function () {
+        return 1
+    }
+}
+
+function func2() {
+    return function () {
+        return 2
+    }
+}
+
+console.log(func1()() + func2()())
