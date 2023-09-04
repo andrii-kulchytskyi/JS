7a32d945cea674ded2f021a6ee924604297f9aad
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2023-09-04 07:44:21 +0400

    js

--- JavaScript/pra/pra2/2.1.js
+++ JavaScript/pra/pra2/2.1.js
@@ -1,5 +1,24 @@
-let str = 'str';
+// №1⊗jsPmFCCl
+//
+// Не подсматривая в мой код, самостоятельно реализуйте такой же счетчик.
 
-(function () {
-    console.log(1);
-})();
+let schet = function () {
+    let num = 1
+    return function () {
+        console.log(num)
+        num++
+        if (num > 5) {
+            num = 1
+        }
+    }
+}()
+schet()
+schet()
+schet()
+schet()
+schet()
+schet()
+//
+// №2⊗jsPmFCCl
+//
+// Модифицируйте работу счетчика так, чтобы он считал до 5, а потом отсчет начинался сначала.
