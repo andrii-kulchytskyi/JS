bbca986d394d8e4fa47529df73ba6cfec1819c25
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2023-10-28 14:30:34 +0400

    js

--- JavaScript/pra/pra2/2.1.js
+++ JavaScript/pra/pra2/2.1.js
@@ -1,8 +1,56 @@
-const x = (...nums) => {
-    let sum = 0
-    for (const num of nums) {
-        sum += num
-    }
-    return sum / nums.length
-}
-console.log(x(1,2,3,4,5,6))
+// №1⊗jsPmDstFP
+//
+// Переделайте следующий код через деструктуризацию согласно изученной теории:
+//
+//     function func(employee) {
+//         let name       = employee[0];
+//         let surname    = employee[1];
+//         let department = employee[2];
+//         let position   = employee[3];
+//         let salary     = employee[4];
+//     }
+//
+// func( ['John', 'Smit', 'development', 'programmer', 2000] );
+// №2⊗jsPmDstFP
+//
+// Переделайте следующий код через деструктуризацию согласно изученной теории:
+//
+//     function func(employee) {
+//         let name     = employee[0];
+//         let surname  = employee[1];
+//         let info     = employee[2];
+//     }
+//
+// func( ['John', 'Smit', 'development', 'programmer', 2000] );
+// №3⊗jsPmDstFP
+//
+// Переделайте следующий код через деструктуризацию согласно изученной теории:
+//
+//     function func(employee) {
+//         let name       = employee[0];
+//         let surname    = employee[1];
+//         let department = employee[2];
+//
+//         let position;
+//         if (arr[3] !== undefined) {
+//             position = arr[3];
+//         } else {
+//             position = 'джуниор';
+//         }
+//     }
+//
+// func( ['John', 'Smit', 'development'] );
+// №4⊗jsPmDstFP
+//
+// Переделайте следующий код через деструктуризацию согласно изученной теории:
+//
+//     function func(department, employee, hired) {
+//         let name     = employee[0];
+//         let surname  = employee[1];
+//
+//         let year  = hired[0];
+//         let month = hired[1];
+//         let day   = hired[2];
+//     }
+//
+// func( 'development', ['John', 'Smit'], [2018, 12, 31] );
